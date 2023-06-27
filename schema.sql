27171b3e2ac82bd6c74ae110ce740f932a759d2b
mahmoud-eltahawy <gemmy9300@gmail.com>
2023-06-27 12:00:04 +0300

    construct db model

--- schema.sql
+++ schema.sql
@@ -1,29 +1,54 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE IF NOT EXISTS client(
+CREATE TABLE IF NOT EXISTS cargo(
        id UUID PRIMARY KEY,
-       the_name VARCHAR(500) NOT NULL,
+       the_name VARCHAR(500),
+       cargo_number  BIGINT
 );
 
+CREATE TABLE IF NOT EXISTS bill(
+       bill_number  BIGINT  PRIMARY KEY,
+       the_date     DATE
+);
 
-CREATE TABLE IF NOT EXISTS cargo(
-       cargo_number  BIGINT  PRIMARY KEY,
-       the_name VARCHAR(500) NOT NULL,
+CREATE TABLE IF NOT EXISTS cargo_bill(
+       id UUID PRIMARY KEY,
+       cargo_id UUID,
+       bill_number BIGINT,
+       quantity BIGINT,
+       one_cost DECIMAL,
+       FOREIGN KEY(cargo_id) REFERENCES cargo(id),
+       FOREIGN KEY(bill_number) REFERENCES bill(bill_number)
 );
 
 CREATE TABLE IF NOT EXISTS buy_bill(
-       pill_number  BIGINT  PRIMARY KEY,
-       client_id    UUID    NOT NULL,
-       the_date     DATE    NOT NULL,
-       tax_number   BIGINT  NOT NULL,
-       the_value    BIGINT  NOT NULL,
-       FOREIGN KEY(client_id) REFERENCES client(id)
+       id UUID PRIMARY KEY,
+       cargo_id UUID,
+       bill_number BIGINT,
+       quantity BIGINT,
+       one_cost DECIMAL,
+       FOREIGN KEY(cargo_id) REFERENCES cargo(id),
+       FOREIGN KEY(bill_number) REFERENCES bill(bill_number)
+);
+
+CREATE TABLE IF NOT EXISTS client(
+       id UUID PRIMARY KEY,
+       the_name VARCHAR(500)
+);
+
+CREATE TABLE IF NOT EXISTS company(
+       id UUID PRIMARY KEY,
+       the_name VARCHAR(500)
 );
 
 CREATE TABLE IF NOT EXISTS sell_bill(
-       pill_number  BIGINT  PRIMARY KEY,
-       the_date     DATE    NOT NULL,
-       tax_number   BIGINT  NOT NULL,
-       the_value    BIGINT  NOT NULL,
+       bill_number BIGINT PRIMARY KEY,
+       tax_number BIGINT,
+       company_id UUID,
+       client_id UUID,
+       total_cost DECIMAL,
+       discount DECIMAL DEFAULT 0,
+       FOREIGN KEY(bill_number) REFERENCES bill(bill_number),
+       FOREIGN KEY(company_id) REFERENCES company(id),
        FOREIGN KEY(client_id) REFERENCES client(id)
 );
